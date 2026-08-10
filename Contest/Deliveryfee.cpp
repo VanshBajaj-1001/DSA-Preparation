b5ef756b966896e27da3ffa9a4a36650344be4a6
Vansh Bajaj <vanshbajaj1001@gmail.com>
2026-08-10 20:25:15 +0530

    DeliveryFee

---
+++ Contest/Deliveryfee.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int A;
+	int B;
+	int k;
+	cin>>A;
+	cin>>B;
+	cin>>k;
+	if(A>=k){
+	    cout<<A<<endl;
+	}
+	else{
+	    cout<<A+B<<endl;
+	}
+return 0;
+}
