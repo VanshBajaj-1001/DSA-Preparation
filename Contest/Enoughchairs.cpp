2180e901348f006dec159c5f51b250d74cbe86f4
Vansh Bajaj <vanshbajaj1001@gmail.com>
2026-08-17 19:39:21 +0530

    Codechef monday contest 1

---
+++ Contest/Enoughchairs.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int n;
+int k;
+int p;
+cin>>n;
+cin>>k;
+cin>>p;
+if(p<=n*k){
+    cout<<"YES";
+}
+else{
+    cout<<"NO";
+}
+return 0;
+}
