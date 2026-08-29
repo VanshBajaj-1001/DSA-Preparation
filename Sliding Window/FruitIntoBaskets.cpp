8bb8c155808d911e95e912217069b6214492d365
Vansh Bajaj <vanshbajaj1001@gmail.com>
2026-08-29 19:13:49 +0530

    Fruit into Baskets

---
+++ Sliding Window/FruitIntoBaskets.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        int ans=0;
+        int l=0;
+        unordered_map<int,int> freq;
+        for(int r=0;r<fruits.size();r++){
+            freq[fruits[r]]++;
+            while(freq.size()>2){
+                freq[fruits[l]]--;
+                if(freq[fruits[l]]==0){
+                    freq.erase(fruits[l]);
+                }
+                l++;
+            }
+            ans=max(ans,r-l+1);
+        }
+        return ans;
+    }
+};
+int main(){
+    Solution obj;
+    int n;
+    cin>>n;
+    vector<int> nums(n);
+    for(int i=0;i<n;i++){
+        cin>>nums[i];
+    }
+    cout<<obj.totalFruit(nums)<<endl;
+    return 0;
+}
